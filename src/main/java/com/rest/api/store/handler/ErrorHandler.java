c4f66442e39a1c448ab4b749dc7de92e8875df46
Wiktor <kasztanators2701@gmail.com>
2024-03-26 16:15:45 +0100

    add ErrorHandler

---
+++ src/main/java/com/rest/api/store/handler/ErrorHandler.java
@@ -0,0 +1,64 @@
+package com.rest.api.store.handler;
+
+import com.rest.api.store.dto.ErrorResponse;
+import com.rest.api.store.exception.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Log4j2
+@ControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        log.error("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(StoreGenericException.class)
+    public ResponseEntity<Object> handleStoreGenericException(Exception ex) {
+        log.error("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(StoreBusinessException.class)
+    public ResponseEntity<Object> handleStoreBusinessException(Exception ex) {
+        log.warn("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(StoreNotFoundException.class)
+    public ResponseEntity<Object> handleStoreRegisterNotFoundException(Exception ex) {
+        log.warn("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleStoreAccessDeniedException(Exception ex) {
+        log.warn("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
+    public ResponseEntity<Object> handleStoreAccessAuthenticationCredentialsNotFoundException(Exception ex) {
+        log.warn("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("There was an error validating the token"));
+    }
+
+    @ExceptionHandler(StoreInvalidCredentialsException.class)
+    public ResponseEntity<Object> handleStoreAccessStoreInvalidCredentialsException(Exception ex) {
+        log.warn("Exception intercepted: caused by " + ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Invalid username/password!"));
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(Exception ex) {
+        log.warn("User already exists " + ex);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse("User already exists!"));
+    }
+
+}
