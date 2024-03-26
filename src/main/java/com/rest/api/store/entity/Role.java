595a255c4fc1427366ca34b9139f93a2ed3cc0da
Wiktor <kasztanators2701@gmail.com>
2024-03-26 16:17:19 +0100

    add role and user entities

---
+++ src/main/java/com/rest/api/store/entity/Role.java
@@ -0,0 +1,40 @@
+package com.rest.api.store.entity;
+
+import com.rest.api.store.enumeration.RoleEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Table(name = "ROLE")
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "role_id", updatable = false, nullable = false)
+    private Long roleID;
+
+    @Column(name = "role")
+    @Enumerated(value = EnumType.STRING)
+    private RoleEnum roleEnum;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            referencedColumnName = "user_id",
+            foreignKey = @ForeignKey(name = "role_user_fk")
+    )
+    private User user;
+
+    public Role(RoleEnum roleEnum) {
+        this.roleEnum = roleEnum;
+    }
+
+}
--- src/main/java/com/rest/api/store/entity/User.java
+++ src/main/java/com/rest/api/store/entity/User.java
@@ -1,14 +1,21 @@
 package com.rest.api.store.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static jakarta.persistence.CascadeType.*;
+import static jakarta.persistence.FetchType.EAGER;
 
 @NoArgsConstructor
 @Data
@@ -17,8 +24,39 @@ import java.util.UUID;
 public class User {
     @Id
     @GeneratedValue
+    @Column(name = "user_id")
     private UUID id;
-    private String userName;
-    private String password;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "account_enable")
+    private boolean enabled;
+
+    @Column(name = "credentials_expired")
+    private boolean credentialsNonExpired;
+
+    @Column(name = "account_expired")
+    private boolean accountNonExpired;
+
+    @Column(name = "account_locked")
+    private boolean locked;
+    @JsonIgnore
+    @OneToMany(cascade = {PERSIST, MERGE, REMOVE}, fetch = EAGER, mappedBy = "user", orphanRemoval = true)
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+        role.setUser(this);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this
+                .roles
+                .stream() //
+                .map(role -> new SimpleGrantedAuthority(role.getRoleEnum().toString()))
+                .collect(Collectors.toSet());
+    }
 }
