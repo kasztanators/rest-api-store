9b99e73d67081ccbad0098d74671c26a82c8d08a
Wiktor <kasztanators2701@gmail.com>
2024-03-27 14:40:51 +0100

    add security AuthEntryPoint and logoutHandler

---
+++ src/main/java/com/rest/api/store/security/CustomLogoutHandler.java
@@ -0,0 +1,29 @@
+package com.rest.api.store.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.stereotype.Component;
+
+@Component(value = "customLogoutHandler")
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private final FindByIndexNameSessionRepository<? extends Session> redisIndexedSessionRepository;
+
+    public CustomLogoutHandler(RedisIndexedSessionRepository redisIndexedSessionRepository) {
+        this.redisIndexedSessionRepository = redisIndexedSessionRepository;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        String id = request.getSession(false).getId();
+        if (id != null && this.redisIndexedSessionRepository.findById(id) != null) {
+            this.redisIndexedSessionRepository.deleteById(id);
+        }
+    }
+
+}
