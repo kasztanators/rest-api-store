55db7ca636756bb329c83865c1109dc085e92335
Wiktor <kasztanators2701@gmail.com>
2024-03-27 13:11:22 +0100

    rename services

--- src/main/java/com/rest/api/store/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.rest.api.store.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
