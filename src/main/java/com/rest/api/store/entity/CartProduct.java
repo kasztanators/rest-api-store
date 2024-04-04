16660df7a3d05878a44fe5d12e188e31ba1cc3a8
Wiktor <kasztanators2701@gmail.com>
2024-04-04 12:01:26 +0200

    fix serialization problem

--- src/main/java/com/rest/api/store/entity/CartProduct.java
+++ src/main/java/com/rest/api/store/entity/CartProduct.java
@@ -3,10 +3,12 @@ package com.rest.api.store.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Entity
 @Table(name = "cart_products")
-public class CartProduct {
+public class CartProduct implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cart_product_id", updatable = false, nullable = false)
